df760ccaec80bef1964ee2818934134b84bb15ed
Joshua Smith <mtgmirage@protonmail.com>
2024-12-09 08:56:51 -0600

    menu implemented

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,15 +1,101 @@
 ﻿
 //create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
+List<Product> products = new List<Product>()
+{
+    new Product()
+    {
+        Name = "Poetry Book 1",
+        Price = 29.99M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "Trumpet",
+        Price = 2499.99M,
+        ProductTypeId = 2
+    },
+    new Product()
+    {
+        Name = "Euphonium",
+        Price = 4999.99M,
+        ProductTypeId = 2
+    },
+    new Product()
+    {
+        Name = "Tuba",
+        Price = 24999.99M,
+        ProductTypeId = 2
+    },
+    new Product()
+    {
+        Name = "Poetry Book 2",
+        Price = 24.99M,
+        ProductTypeId = 1
+    }
+};
 
 //create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
+List<ProductType> productTypes = new List<ProductType>()
+{
+    new ProductType()
+    {
+        Title = "Poetry Book",
+        Id = 1
+    },
+    new ProductType()
+    {
+        Title = "Brass Instrument",
+        Id = 2
+    }
+};
 
 //put your greeting here
+Console.Clear();
+Console.WriteLine("Welcome to Brass & Poem!");
 
 //implement your loop here
+DisplayMenu();
 
 void DisplayMenu()
 {
-    throw new NotImplementedException();
+    string choice = null;
+    while (choice != "5")
+    {
+        Console.WriteLine(@"-- Choose an option:
+            1. Display all products
+            2. Delete a product
+            3. Add a new product
+            4. Update product properties
+            5. Exit"
+        );
+
+        choice = Console.ReadLine();
+        Console.Clear();
+        if (choice == "1")
+        {
+            DisplayAllProducts(products, productTypes);
+        }
+        else if (choice == "2")
+        {
+            DeleteProduct(products, productTypes);
+        }
+        else if (choice == "3")
+        {
+            AddProduct(products, productTypes);
+        }
+        else if (choice == "4")
+        {
+            UpdateProduct(products, productTypes);
+        }
+        else if (choice == "5")
+        {
+            Console.WriteLine("Goodbye!");
+        }
+        else 
+        {
+            Console.WriteLine("Invalid Input!");
+        }
+    }
 }
 
 void DisplayAllProducts(List<Product> products, List<ProductType> productTypes)
